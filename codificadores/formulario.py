5d358d1ca74c67beb096fe2f71111369039d4309
Santy2610 <santy2610@gmail.com>
2024-03-14 20:35:41 -0400

    produccion modificada 14 de marzo

--- codificadores/formulario.py
+++ codificadores/formulario.py
@@ -1,29 +1,29 @@
 from django import forms
 
 class formulariounidadm(forms.Form):
-    Descripcion=forms.CharField()
+    Descripcion=forms.CharField(label="Descripción")
     Unidad=forms.CharField()
 
 class formularioalmacen(forms.Form):
     Codigo=forms.CharField()
-    Descripcion=forms.CharField()
+    Descripcion=forms.CharField(label="Descripción")
 
 class formulariocosto(forms.Form):
     Codigo=forms.CharField()
-    Descripcion=forms.CharField() 
+    Descripcion=forms.CharField(label="Descripción") 
 
 class formularioareas(forms.Form):
     Codigo=forms.CharField()
-    Descripcion=forms.CharField() 
+    Descripcion=forms.CharField(label="Descripción") 
        
 class formularioequipos(forms.Form):
     Codigo=forms.CharField()
-    Descripcion=forms.CharField()
+    Descripcion=forms.CharField(label="Descripción")
 
 class formulariocausas(forms.Form):
     Codigo=forms.CharField()
-    Descripcion=forms.CharField()
+    Descripcion=forms.CharField(label="Descripción")
 
 class formulariotrab(forms.Form):
     codigo=forms.CharField()
-    nombre=forms.CharField()
+    nombre=forms.CharField(label="Descripción")
