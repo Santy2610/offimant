72236e86d38d1b85ce6e7504399a057df40fbbdd
Santy2610 <santy2610@gmail.com>
2024-04-21 17:30:33 -0400

    Paguina principal

--- Offimant/views.py
+++ Offimant/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, redirect
+from ordenes.models import orden
+from producciones.models import producciones
+from vales.models import vale
+from django.db.models import Sum, Count
 
 def principal(request):
-      return render(request,"index.html")
+      listorden=orden.objects.all().order_by('-codigo')[0:5]
+      listvale=vale.objects.values('costo').order_by('costo').annotate(cant=Count('costo'))
+      listprod=producciones.objects.values('descripcion','unidad').order_by('descripcion').annotate(prosu=Sum('cantidad'))
+      return render(request,"index.html", {"listord":listorden, "listpro":listprod, "listv":listvale})
