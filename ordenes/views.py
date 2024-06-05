8d8e470034372892a37ff9449e89fa9480704027
Santy2610 <santy2610@gmail.com>
2024-06-05 18:38:43 -0400

    5 de junio

--- ordenes/views.py
+++ ordenes/views.py
@@ -38,77 +38,77 @@ def ordenesnew(request, vista, dato, page):
        return render(request,"adicionarordenes.html",{"form":formorden, "resultsw":result,"fallasw":falla, "vistasw":vista, "datosw":dato, "pagesw":page, "clogsw":clog})
 
 def ordenesadd(request):
-      Codigof=request.GET["Codigo"]
-      Departamentof=request.GET["Departamento"]
-      areaf=request.GET["area"]
-      equipof=request.GET["equipo"]
-      idequipof=request.GET["idequipo"]
-      fechaRepf=request.GET["fechaRep"]
-      motivosolicf=request.GET["motivosolic"]
-      fallaf=request.GET["Falla"]
-      solicitantef=request.GET["solicitante"]
-      afectacionf=request.GET["afectacion"]
-      prioridadf=request.GET["prioridad"]
-      Resultadof=request.GET["Resultado"]
-      reparadof=request.GET["reparado"]
-      valef=request.GET["valecod"]
-      fechaejef=request.GET["fechaEje"]
-      destinof=request.GET["Destino"]
-      culminadaf=request.GET["Culminada"]
-      campa=request.GET["Campa"]
-      ordenlist=orden.objects.create(codigo=Codigof, departamento=Departamentof, area=areaf,
+     Codigof=request.GET["Codigo"]
+     Departamentof=request.GET["Departamento"]
+     areaf=request.GET["area"]
+     equipof=request.GET["equipo"]
+     idequipof=request.GET["idequipo"]
+     fechaRepf=request.GET["fechaRep"]
+     motivosolicf=request.GET["motivosolic"]
+     fallaf=request.GET["Falla"]
+     solicitantef=request.GET["solicitante"]
+     afectacionf=request.GET["afectacion"]
+     prioridadf=request.GET["prioridad"]
+     Resultadof=request.GET["Resultado"]
+     reparadof=request.GET["reparado"]
+     valef=request.GET["valecod"]
+     fechaejef=request.GET["fechaEje"]
+     destinof=request.GET["Destino"]
+     culminadaf=request.GET["Culminada"]
+     campa=request.GET["Campa"]
+     ordenlist=orden.objects.create(codigo=Codigof, departamento=Departamentof, area=areaf,
                                       equipo=equipof, idequipo=idequipof, fechaRep=fechaRepf,
                                       motivosolic=motivosolicf, falla=fallaf, solicitante=solicitantef,
                                       Afectacion=afectacionf, prioridad=prioridadf, Resumen=Resultadof,
                                       Reparado=reparadof,valecodigo=valef, Destino=destinof, fechaEje=fechaejef,
                                       culminada=culminadaf, campa=campa)
-      return redirect(ordenes)
+     return redirect(ordenes)
 
 def ordenesupdate(request, dato, page):
-      Codigof=request.GET["Codigo"]
-      Departamentof=request.GET["Departamento"]
-      areaf=request.GET["area"]
-      equipof=request.GET["equipo"]
-      idequipof=request.GET["idequipo"]
-      fechaRepf=request.GET["fechaRep"]
-      motivosolicf=request.GET["motivosolic"]
-      fallaf=request.GET["Falla"]
-      solicitantef=request.GET["solicitante"]
-      afectacionf=request.GET["afectacion"]
-      prioridadf=request.GET["prioridad"]
-      Resultadof=request.GET["Resultado"]
-      reparadof=request.GET["reparado"]
-      valef=request.GET["valecod"]
-      Destinof=request.GET["Destino"]
-      fechaejef=request.GET["fechaEje"]
-      culminadaf=request.GET["Culminada"]
-      campa=request.GET["Campa"]
-      ordenlist=orden.objects.get(pk=dato)
-      ordenlist.codigo=Codigof
-      ordenlist.departamento=Departamentof
-      ordenlist.area=areaf
-      ordenlist.equipo=equipof
-      ordenlist.idequipo=idequipof
-      ordenlist.fechaRep=fechaRepf
-      ordenlist.motivosolic=motivosolicf
-      ordenlist.falla=fallaf
-      ordenlist.solicitante=solicitantef
-      ordenlist.Afectacion=afectacionf
-      ordenlist.prioridad=prioridadf
-      ordenlist.fechaEje=fechaejef
-      ordenlist.Resumen=Resultadof
-      ordenlist.Reparado=reparadof
-      ordenlist.Destino=Destinof
-      ordenlist.valecodigo=valef
-      ordenlist.culminada=culminadaf
-      ordenlist.campa=campa
-      ordenlist.save()
-      return redirect("/ordenes/?page=%s" %page)
+     Codigof=request.GET["Codigo"]
+     Departamentof=request.GET["Departamento"]
+     areaf=request.GET["area"]
+     equipof=request.GET["equipo"]
+     idequipof=request.GET["idequipo"]
+     fechaRepf=request.GET["fechaRep"]
+     motivosolicf=request.GET["motivosolic"]
+     fallaf=request.GET["Falla"]
+     solicitantef=request.GET["solicitante"]
+     afectacionf=request.GET["afectacion"]
+     prioridadf=request.GET["prioridad"]
+     Resultadof=request.GET["Resultado"]
+     reparadof=request.GET["reparado"]
+     valef=request.GET["valecod"]
+     Destinof=request.GET["Destino"]
+     fechaejef=request.GET["fechaEje"]
+     culminadaf=request.GET["Culminada"]
+     campa=request.GET["Campa"]
+     ordenlist=orden.objects.get(pk=dato)
+     ordenlist.codigo=Codigof
+     ordenlist.departamento=Departamentof
+     ordenlist.area=areaf
+     ordenlist.equipo=equipof
+     ordenlist.idequipo=idequipof
+     ordenlist.fechaRep=fechaRepf
+     ordenlist.motivosolic=motivosolicf
+     ordenlist.falla=fallaf
+     ordenlist.solicitante=solicitantef
+     ordenlist.Afectacion=afectacionf
+     ordenlist.prioridad=prioridadf
+     ordenlist.fechaEje=fechaejef
+     ordenlist.Resumen=Resultadof
+     ordenlist.Reparado=reparadof
+     ordenlist.Destino=Destinof
+     ordenlist.valecodigo=valef
+     ordenlist.culminada=culminadaf
+     ordenlist.campa=campa
+     ordenlist.save()
+     return redirect("/ordenes/?page=%s" %page)
 
 def ordenesdel(request, dato):
-      ordenlist=orden.objects.get(pk=dato)
-      ordenlist.delete()
-      return redirect(ordenes)
+     ordenlist=orden.objects.get(pk=dato)
+     ordenlist.delete()
+     return redirect(ordenes)
 
 def ordenimpcont(request):
      ordenimp=orden.objects.all().order_by('codigo')
